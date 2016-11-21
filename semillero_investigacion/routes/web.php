d6105f4495e4ef17fb0fd11b0f66355897801cb7
linuxero96 <al-felipe@hotmail.com>
2016-11-20 20:41:07 -0500

    Experiencia laboral controller

--- semillero_investigacion/routes/web.php
+++ semillero_investigacion/routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 Route::resource('perfec', 'PerfeccionamientoController');
+Route::resource('experiencia', 'ExperienciaLabController');
