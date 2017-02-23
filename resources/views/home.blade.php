f4bf73813ba40ae06b5ece075b220f26c7c4ba73
Sebastián Pachón <sebaspachon94@gmail.com>
2017-02-22 20:20:18 -0500

    home updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,7 +73,7 @@
                 <hr>
                 <div class="information-container">
                     <h2>Idiomas</h2>
-                    <table>
+                    <table class="table table-striped">
                         <thead>
                             <tr>
                                 <th>Nombre</th>
@@ -95,73 +95,127 @@
                 </div>
                 <hr>
                 <div class="information-container">
-                    <h2>Información Personal</h2>
-                    <p><b>Nombre: </b>{{ Auth::user()->name }}</p>
-                    <p><b>Apellido: </b>{{ Auth::user()->apellidos }}</p>
-                    <p><b>Género: </b> Mujer</p>
-                    <p><b>Estado Civil: </b> Andrés</p>
-                    <p><b>Nacionalidad: </b> Colombia</p>
-                    <p><b>Libreta Militar: </b> Sí</p>
-                    <p><b>Fecha de Nacimiento: </b> 20 de Octubre de 1995</p>
-                    <p><b>Lugar de Nacimiento: </b> Fusagasugá</p>
-                    <p><b>Dirección: </b> Carrera 12312 #123 - 132</p>
-                    <p><b>E-mail: </b> linuxdiaz@gmail.com</p>
+                    <h2>Perfeccionamiento</h2>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Entidad</th>
+                                <th>Fecha de Inicio</th>
+                                <th>Fecha de Terminación</th>
+                                <th>Intensidad Horaria</th>
+                                <th>Puntaje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>U Nacional</td>
+                                <td>14 de Octubre de 2001</td>
+                                <td>15 de Mayo de 2002</td>
+                                <td>120 horas</td>
+                                <td>60/80</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <hr>
                 <div class="information-container">
-                    <h2>Información Personal</h2>
-                    <p><b>Nombre: </b>{{ Auth::user()->name }}</p>
-                    <p><b>Apellido: </b>{{ Auth::user()->apellidos }}</p>
-                    <p><b>Género: </b> Mujer</p>
-                    <p><b>Estado Civil: </b> Andrés</p>
-                    <p><b>Nacionalidad: </b> Colombia</p>
-                    <p><b>Libreta Militar: </b> Sí</p>
-                    <p><b>Fecha de Nacimiento: </b> 20 de Octubre de 1995</p>
-                    <p><b>Lugar de Nacimiento: </b> Fusagasugá</p>
-                    <p><b>Dirección: </b> Carrera 12312 #123 - 132</p>
-                    <p><b>E-mail: </b> linuxdiaz@gmail.com</p>
+                    <h2>Formación Académica</h2>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nivel</th>
+                                <th>Modalidad</th>
+                                <th>Nombre</th>
+                                <th>Número de Semestres</th>
+                                <th>Graduado</th>
+                                <th>Institución</th>
+                                <th>Tarjeta Profesional</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Pregrado</td>
+                                <td>Presencial</td>
+                                <td>Ingeniería de Sistemas</td>
+                                <td>10</td>
+                                <td>Sí</td>
+                                <td>U Nacional</td>
+                                <td>123321</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <hr>
                 <div class="information-container">
-                    <h2>Información Personal</h2>
-                    <p><b>Nombre: </b>{{ Auth::user()->name }}</p>
-                    <p><b>Apellido: </b>{{ Auth::user()->apellidos }}</p>
-                    <p><b>Género: </b> Mujer</p>
-                    <p><b>Estado Civil: </b> Andrés</p>
-                    <p><b>Nacionalidad: </b> Colombia</p>
-                    <p><b>Libreta Militar: </b> Sí</p>
-                    <p><b>Fecha de Nacimiento: </b> 20 de Octubre de 1995</p>
-                    <p><b>Lugar de Nacimiento: </b> Fusagasugá</p>
-                    <p><b>Dirección: </b> Carrera 12312 #123 - 132</p>
-                    <p><b>E-mail: </b> linuxdiaz@gmail.com</p>
+                    <h2>Escalafón</h2>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Tipo</th>
+                                <th>Certificación</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Maestro</td>
+                                <td><a href="">Ver</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <hr>
                 <div class="information-container">
-                    <h2>Información Personal</h2>
-                    <p><b>Nombre: </b>{{ Auth::user()->name }}</p>
-                    <p><b>Apellido: </b>{{ Auth::user()->apellidos }}</p>
-                    <p><b>Género: </b> Mujer</p>
-                    <p><b>Estado Civil: </b> Andrés</p>
-                    <p><b>Nacionalidad: </b> Colombia</p>
-                    <p><b>Libreta Militar: </b> Sí</p>
-                    <p><b>Fecha de Nacimiento: </b> 20 de Octubre de 1995</p>
-                    <p><b>Lugar de Nacimiento: </b> Fusagasugá</p>
-                    <p><b>Dirección: </b> Carrera 12312 #123 - 132</p>
-                    <p><b>E-mail: </b> linuxdiaz@gmail.com</p>
+                    <h2>Producción</h2>
+                     <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Categoría</th>
+                                <th>Tipo</th>
+                                <th>Certificado</th>
+                                <th>Fecha</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>III</td>
+                                <td>Articulo</td>
+                                <td><a href="">ver</a></td>
+                                <td>14 de Mayo de 2010</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <hr>
                 <div class="information-container">
-                    <h2>Información Personal</h2>
-                    <p><b>Nombre: </b>{{ Auth::user()->name }}</p>
-                    <p><b>Apellido: </b>{{ Auth::user()->apellidos }}</p>
-                    <p><b>Género: </b> Mujer</p>
-                    <p><b>Estado Civil: </b> Andrés</p>
-                    <p><b>Nacionalidad: </b> Colombia</p>
-                    <p><b>Libreta Militar: </b> Sí</p>
-                    <p><b>Fecha de Nacimiento: </b> 20 de Octubre de 1995</p>
-                    <p><b>Lugar de Nacimiento: </b> Fusagasugá</p>
-                    <p><b>Dirección: </b> Carrera 12312 #123 - 132</p>
-                    <p><b>E-mail: </b> linuxdiaz@gmail.com</p>
+                    <h2>Experiencia Laboral</h2>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Cargo</th>
+                                <th>Entidad</th>
+                                <th>Dirección</th>
+                                <th>Ciudad</th>
+                                <th>Teléfono</th>
+                                <th>Correo</th>
+                                <th>Tipo</th>
+                                <th>Fecha Inicio</th>
+                                <th>Fecha Terminación</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Asistente</td>
+                                <td>Alcaldía</td>
+                                <td>Carrera 5</td>
+                                <td>Fusagasugá</td>
+                                <td>867 55 56</td>
+                                <td>melopela@gmail.com</td>
+                                <td>II</td>
+                                <td>4 de Octubre de 2008</td>
+                                <td>5 de Noviembre de 2012</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
