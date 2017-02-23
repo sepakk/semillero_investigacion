ddd8b5db339348124efca41a38408604c706ebf3
Sebastián Pachón <sebaspachon94@gmail.com>
2017-02-22 20:43:06 -0500

    added bootstrap

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
 		Banco de Hojas de Vida - Universidad de Cundinamarca
 	</title>
 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 					
 </head>
@@ -13,5 +14,11 @@
 		<img src="image_logo.png">
 		@yield('contenido')
 	</div>
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
