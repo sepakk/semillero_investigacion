dbb11d9b1cbeba50571aeee51f68adc02c969247
Sepakk <sebaspachon94@gmail.com>
2017-03-15 19:51:55 -0500

    escalafon mejorado

--- resources/views/escalafon/create.blade.php
+++ resources/views/escalafon/create.blade.php
@@ -14,34 +14,37 @@
 					<h2>Universidad de Cundinamarca</h2>
 
 					{!!Form::open(array('url'=>'escalafones','method'=>'POST','autocomplete'=>'off','files'=>true, 'class'=>'big-form'))!!}
-	                    {{Form::token()}}
-						<label for="name">CATEGORIA EN EL ESCALAFON</label>
-						<label for="name">Si ha sido escalonado previamente en una Universidad y está ubicado en una de las categorías de las relacionadas a continuación identifíquela y anexe documento de soporte.</label>
-				            
-				            <div class="form-group{{ $errors->has('tipo_escalafon') ? ' has-error' : '' }}">
-		                         <div class="contenedor-lugar">
-				 					<label for="tipo_escalafon" >Tipo:</label>
-		                         	<select id="tipo_escalafon" name="tipo_escalafon">
-							             @foreach($tipos as $tipo)
-							             	<option value="{{$tipo->cod_escalafon}}">{{$tipo->nombre_escalafon}}</option>
-							             @endforeach
-						             </select>
-					             </div>
-				            </div>
+						{{Form::token()}}
+						<div class="duplicate">
+							<label for="name">CATEGORIA EN EL ESCALAFON</label>
+							<label for="name">Si ha sido escalonado previamente en una Universidad y está ubicado en una de las categorías de las relacionadas a continuación identifíquela y anexe documento de soporte.</label>
 
-				            <div class="form-group{{ $errors->has('anexo') ? ' has-error' : '' }}">
-					            <div class="contenedor-lugar">
-									 <label for="select_cargos" >Certificado:</label>
-			                    	 <input type="file" name="anexo" id="anexo">
-			                    	 <input type="text" name="documento_identificacion" id="documento_identificacion" value="{{$documento_identificacion}}" class="hidden">
-				            	</div>
-				            </div>
-				            <div class="form-group{{ $errors->has('enviar') ? ' has-error' : '' }}">
-		                    	<input type="submit" name="submit" value="Continuar">
-				            </div>
-				            <div class="form-group{{ $errors->has('tipo') ? ' has-error' : '' }}">
-			                    <input type="submit" name="submit" value="Agregar otro Escalofon">
-			                </div>
+							<div class="form-group{{ $errors->has('tipo_escalafon') ? ' has-error' : '' }}">
+								<div class="contenedor-lugar">
+									<label for="tipo_escalafon" >Tipo:</label>
+									<select id="tipo_escalafon" name="tipo_escalafon">
+										@foreach($tipos as $tipo)
+											<option value="{{$tipo->cod_escalafon}}">{{$tipo->nombre_escalafon}}</option>
+										@endforeach
+									</select>
+								</div>
+							</div>
+
+							<div class="form-group{{ $errors->has('anexo') ? ' has-error' : '' }}">
+								<div class="contenedor-lugar">
+									<label for="select_cargos" >Certificado:</label>
+									<input type="file" name="anexo" id="anexo">
+									<input type="text" name="documento_identificacion" id="documento_identificacion" value="{{$documento_identificacion}}" class="hidden">
+								</div>
+							</div>
+							<hr>
+							<br><br>
+						</div>
+						<button type="button" id="add-more" name="submit">Agregar otro Escalafon</button>
+						
+						<div class="form-group{{ $errors->has('enviar') ? ' has-error' : '' }}">
+							<input type="submit" name="submit" value="Continuar">
+						</div>
 					{!!Form::close()!!}
 				</div>
 			</div>
