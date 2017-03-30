5f2ad45624279bd09402fd1d1e32437d0ce57aed
Sepakk <sebaspachon94@gmail.com>
2017-03-29 20:48:18 -0500

    responsive

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,44 +1,51 @@
 <ul>
-                <li>
-                    <a href="/home">Inicio</a>
-                </li>
-                
-                <li>
-                    <a href="/informacion">Información Personal</a>
-                </li>
-                @if($usuario->tipoUsuario!=1)
-                <li>
-                    <a href="/idioma">Idiomas</a>
-                </li>
+    <li>
+        <button id="collapse" class="menu-collapse">
+            <span class="glyphicon glyphicon-menu-hamburger"></span> Menú
+        </button>
+    </li>
 
-                <li>
-                    <a href="/perfeccionamiento">Perfeccionamiento</a>
-                </li>
+    <li>
+        <a href="/home">Inicio</a>
+    </li>
 
-                <li>
-                    <a href="/formacion">Formación Académica</a>
-                </li>
+    <li>
+        <a href="/informacion">Información Personal</a>
+    </li>
 
-                <li>
-                    <a href="/escalafones">Escalafón</a>
-                </li>
+    @if($usuario->tipoUsuario!=1)
+        <li>
+            <a href="/idioma">Idiomas</a>
+        </li>
 
-                <li>
-                    <a href="/experiencia">Experiencia Laboral</a>
-                </li>
+        <li>
+            <a href="/perfeccionamiento">Perfeccionamiento</a>
+        </li>
 
-                <li>
-                    <a href="/producciones">Producción</a>
-                </li>
-                @endif
-                <li>
-                    <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Salir
-                                        </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                </li>
-            </ul>
+        <li>
+            <a href="/formacion">Formación Académica</a>
+        </li>
+
+        <li>
+            <a href="/escalafones">Escalafón</a>
+        </li>
+
+        <li>
+            <a href="/experiencia">Experiencia Laboral</a>
+        </li>
+
+        <li>
+            <a href="/producciones">Producción</a>
+        </li>
+    @endif
+    <li>
+        <a href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+            Salir
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+        </form>
+    </li>
+</ul>
